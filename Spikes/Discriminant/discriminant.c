3c6d0a2b691dab4c0c65168ca8b94fa4c069ffb8
Kenneth Rivard <kenneth.j.rivard@wmich.edu>
2017-10-04 12:17:51 -0400

    add changed to discriminant spike

--- Spikes/Discriminant/discriminant.c
+++ Spikes/Discriminant/discriminant.c
@@ -7,11 +7,24 @@
 #include <assert.h>
 #include "../../quadhead.h"
 
-int Main(int argc, char * argv[])
+int main(int argc, char * argv[])
 {
-	assert(argc == 4);
-	double desc = getDisc(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+	
+	//printf("%lf", atof(argv[1]));
+
+	assert(argc == 5);
+
+
+	int a = atof(argv[1]);
+	int b = atof(argv[2]);
+	int c = atof(argv[3]);	
+
+	double det = (b*b) - (4*a*c); //getDisc(atof(argv[1]), atof(argv[2]), atof(argv[3]));
 	assert(det == det);
+
+	//printf("\n%lf and %lf\n", atof(argv[4]), det);
+
+	assert(atof(argv[4]) == det);
 
 	return 0;
 }
