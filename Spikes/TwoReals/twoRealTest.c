c97acc0f4606fb88a53455ffd4d3a1ec10a09dca
theBKBroiler <kenneth.j.rivard@wmich.edu>
2017-10-10 15:06:49 -0400

    fixed typo in TPS_10092017

---
+++ Spikes/TwoReals/twoRealTest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "../../quadhead.h"
+
+int main(int argc, char *argv[])
+{
+	assert(argc == 4);
+	double* reals = twoReal(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+	assert(reals != NULL);
+
+	return 0;
+}
